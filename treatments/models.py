9d0ac42ee3d31762909d542f68cb6fba2fb1edb3
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-11-28 21:27:21 +0000

    Create treatments database

--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Inactive"), (1, "Active"))
 
 # Create your models here.
+class Post(models.Model):
+    category = models.CharField(max_length=200)
+    treatment = models.CharField(max_length=200)
+    treatment_id = models.IntegerField()
+    description = models.TextField()
+    duration = models.CharField(max_length=50)
+    cost = models.CharField(max_length=50)
+    status = models.IntegerField(choices=STATUS, default=0)
