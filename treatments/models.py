c031e119274109143eaf126174ecb1ef69178f17
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-11-28 22:46:04 +0000

    Update db name and troubleshoot issues with change until treatment list displayed in admin panel.

--- treatments/models.py
+++ treatments/models.py
@@ -7,8 +7,15 @@ STATUS = ((0, "Inactive"), (1, "Active"))
 class Treatment(models.Model):
     category = models.CharField(max_length=200)
     treatment = models.CharField(max_length=200)
-    treatment_id = models.IntegerField()
+    treatment_id = models.CharField(max_length=50)
     description = models.TextField()
     duration = models.CharField(max_length=50)
     cost = models.CharField(max_length=50)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        db_table = 'treatments_treatment'
+        ordering = ["category"]
+
+    def __str__(self):
+        return f"{self.category} : {self.treatment}"
