8959953062d3ebb99b3301461f5dff22a538741e
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-11-28 20:41:55 +0000

    Add secret key

--- serenity_escape/settings.py
+++ serenity_escape/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7-&8skx36e@fvwxsvb45rocja1q81&e3l%1#90no@1!t(g1gf*'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
