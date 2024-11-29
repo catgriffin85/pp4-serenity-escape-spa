76691cf0f42fd58cee1b8fd3429aee8ccbf550e7
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-11-29 21:37:02 +0000

    Update treatments_page.html file to improve format

--- treatments/views.py
+++ treatments/views.py
@@ -4,6 +4,6 @@ from .models import Treatment
 
 # Create your views here.
 class TreatmentList(generic.ListView):
-    queryset = Treatment.objects.all()
+    queryset = Treatment.objects.all().filter(status=1).order_by("category")
     template_name = "treatments_page.html"
     context_object_name = "treatments"
