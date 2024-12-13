dd387c915cb7e254767f95e9e936541e2e9bf7a5
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-12-13 16:59:22 +0000

    Fix issue with reviews displaying on homepage

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,6 @@
 from . import views
 from django.urls import path
 from .views import homepage_view
-from book_now.models import Review
-from book_now.views import customer_review
 
 urlpatterns = [
     path('', views.homepage_view, name='home'),
--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import ListView
 from book_now.models import Review
 from book_now.views import customer_review
 
@@ -10,13 +11,6 @@ def homepage_view(request):
         request,"homepage/homepage.html"
         )
 
-#def customer_review(request):
-#    reviews = Review.objects.all().order_by('created_on')
-#    paginator = Paginator(reviews, 1)
-#    return render(request, 'homepage.html', {'reviews': reviews})
-
-class ReviewList(generic.ListView):
-    queryset = Review.objects.all().order_by("created_on")
-    template_name = "review.html"
-    paginate_by = 1
-    context_object_name = "reviews"
+def homepage_view(request):
+    reviews = Review.objects.all().order_by('created_on')[:5]
+    return render(request, 'homepage/homepage.html', {'reviews': reviews})
