137a20900a1bfde50ff65a83a2c56131958827e4
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-12-20 22:44:36 +0000

    Install cloudinary and updated link for homepage image

--- serenity_escape/settings.py
+++ serenity_escape/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-catgriffin8-pp4serenity-dmurijy25dc.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'homepage',
     'treatments',
     'book_now',
